c78a4814353ca01a20aeeb1c5a06539be606235b
aaronreesluker <aaron@revivemarketing.uk>
2026-01-10 12:39:08 +0000

    Simplify webpack config: media scan route disabled

--- next.config.mjs
+++ next.config.mjs
@@ -16,31 +16,8 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer, webpack }) => {
-    if (isServer) {
-      // Prevent Next.js from analyzing static paths in API routes
-      // This prevents large directories from being bundled into serverless functions
-      config.plugins.push(
-        new webpack.IgnorePlugin({
-          resourceRegExp: /^\.\/public\/videos\/instagram/,
-        })
-      );
-      
-      // Exclude fs/promises and path from being analyzed for static imports
-      config.externals = config.externals || [];
-      if (Array.isArray(config.externals)) {
-        config.externals.push({
-          'fs/promises': 'commonjs fs/promises',
-          'path': 'commonjs path',
-        });
-      }
-    }
-    return config;
-  },
-  // Exclude filesystem packages from server component analysis
-  experimental: {
-    serverComponentsExternalPackages: ['fs/promises'],
-  },
+  // Media scan route has been disabled to prevent bundling issues
+  // No special webpack config needed since the route is disabled
 };
 
 export default nextConfig;
