2c9ebb6dcb12f6f86d52589f80e0db08383b6ba8
aaronreesluker <aaron@revivemarketing.uk>
2026-01-12 22:43:27 +0000

    Make logo even larger in navigation header

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -124,9 +124,9 @@ export default function Navigation() {
             <Image 
               src="/assets/vrs4.png" 
               alt="VRS Vehicle Rejuvenation Specialists" 
-              width={180}
-              height={48}
-              className="h-12 w-auto"
+              width={240}
+              height={64}
+              className="h-16 w-auto"
               style={{ filter: textColor === "#ffffff" ? "brightness(0) invert(1)" : "none" }}
               priority
             />
